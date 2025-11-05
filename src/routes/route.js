ae6b8cf5bc48087ecb794d219a8d6fef20f45e24
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-11-04 22:22:34 -0300

    Mudanças na tela que inicía o app

--- src/routes/route.js
+++ src/routes/route.js
@@ -57,7 +57,7 @@ function DrawerRoutes() {
 
 export default function Route() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Drawer" component={DrawerRoutes} />
